ab2d3fa901ef699c1da2f461c6d7fa9b6b8e9eab
kgraves <kgraves9@mail.csuchico.edu>
2013-06-17 00:26:18 -0700

    adds db.py.bak which is a backup of the original db module. starts migrating db.py to sqlite3 :)

--- db.py
+++ db.py
@@ -1,78 +1,132 @@
-import MySQLdb as mdb
+import sqlite3
 from datetime import datetime,timedelta
+
 class DB:
-  age = datetime.now()
+
+  def __init__(self, filename)
+    self.filename = filename
+    self.age = datetime.now()
 
   def _open(self):
-    self.con = mdb.connect("localhost","pybot","1q2w3e4r","pybot")
-    self.cur = self.con.cursor()
+    self.conn = sqlite3.connect(self.filename)
+    self.cur  = self.conn.cursor()
+
+    # self.con = mdb.connect("localhost","pybot","1q2w3e4r","pybot")
+    # self.cur = self.con.cursor()
   
   def _close(self):
-    self.con = None
+    self.conn = None
     self.cur.close()
 
-  # should prevent mysql has gone away errors.. ideally
-  def _handle(self):
-    global cur
-    global age
-    now = datetime.now()
-    if now - self.age > timedelta(minutes=5):
-      self.cur.close()
-      self.con = mdb.connect("localhost","pybot","1q2w3e4r","pybot")
-      self.cur = self.con.cursor()
+    # self.con = None
+    # self.cur.close()
+
+  # IS NOT USED ANYWHERE, NOT IMPLEMENTING AT THIS TIME 
+  def _handle(self): global cur
+    return None
+
+    # global age
+    # now = datetime.now()
+    # if now - self.age > timedelta(minutes=5):
+      # self.cur.close()
+      # self.con = mdb.connect("localhost","pybot","1q2w3e4r","pybot")
+      # self.cur = self.con.cursor()
 
   def select(self, where, what):
     try:
       self._open()
-      self.cur.execute("""SELECT %s FROM %s""")
+      self.cur.execute("SELECT %s FROM %s" % (what, where))
       data = self.cur.fetchall()
       self._close()
     except:
+      data = None
       self._close()
-      return None
 
     return data
+
+    # try:
+      # self._open()
+      # self.cur.execute("""SELECT %s FROM %s""")
+      # data = self.cur.fetchall()
+      # self._close()
+    # except:
+      # self._close()
+      # return None
+
+    # return data
 
   def replace(self, where, which, what):
     try:
       self._open()
-      self.cur.execute("""REPLACE INTO %s (%s) VALUES (%s)""",(where, which, what)) 
-      self._close()
+      self.cur.execute("REPLACE INTO %s (%s) VALUES (%s)" % (where, which, what))
     except:
-      self._close()
-      return None
+      # possibly log error or something???
+      pass
 
+    self._close()
+    return None
+
+    # return data
+    # try:
+      # self._open()
+      # self.cur.execute("""REPLACE INTO %s (%s) VALUES (%s)""",(where, which, what)) 
+      # self._close()
+    # except:
+      # self._close()
+      # return None
+
+  # IS NOT USED ANYWHERE, NOT IMPLEMENTING AT THIS TIME 
   def e(self, sql):
-    try:
-      self._open()
-      self.cur.execute(sql) 
-      if "INSERT" in sql or "REPLACE" in sql:
-        self.con.commit()
-        self._close()
-      elif "SELECT" in sql:
-        e = self.cur.fetchall()
-        self._close()
-        return e
-    except Exception, e:
-      print e
-      self.con.rollback()
-      self._close()
-      return None
+    return None
+
+    # try:
+      # self._open()
+      # self.cur.execute(sql) 
+      # if "INSERT" in sql or "REPLACE" in sql:
+        # self.con.commit()
+        # self._close()
+      # elif "SELECT" in sql:
+        # e = self.cur.fetchall()
+        # self._close()
+        # return e
+    # except Exception, e:
+      # print e
+      # self.con.rollback()
+      # self._close()
+      # return None
 
   def insert(self, where, which, what):
     try:
       self._open()
-      self.cur.execute("""INSERT INTO %s (%s) VALUES (%s)""",(where, which, what)) 
-      self._close()
+      self.cur.execute("INSERT INTO %s (%s) VALUES (%s)" % (where, which, what))
     except:
-      self._close()
-      return None
+      # possibly log error or something???
+      pass
 
-  def updateSeen(self,who,statement,event):
-    self._open()
-    #print "executing REPLACE INTO seen (user_name, statement, event) VALUES ( " + str(who) + " " + str(statement) + " " + str(event) + ")"
-    self.cur.execute("REPLACE INTO seen (user_name, statement, event) VALUES (%s, %s, %s)", (who, statement, event))
     self._close()
+    return None
+
+    # try:
+      # self._open()
+      # self.cur.execute("""INSERT INTO %s (%s) VALUES (%s)""",(where, which, what)) 
+      # self._close()
+    # except:
+      # self._close()
+      # return None
+
+  def updateSeen(self, who, statement, event):
+    # formats data and uses the replace method
+
+    which = 'user_name, statement, event'
+    what = str(who) + ' ' + str(statement) + ' ' str(event)
+    self.replace('seen', which, what)
+
+    return None
+
+    # self._open()
+    # #print "executing REPLACE INTO seen (user_name, statement, event) VALUES ( " + str(who) + " " + str(statement) + " " + str(event) + ")"
+    # self.cur.execute("REPLACE INTO seen (user_name, statement, event) VALUES (%s, %s, %s)", (who, statement, event))
+    # self._close()
 
   def getSeen(self, who):
     self._open()
